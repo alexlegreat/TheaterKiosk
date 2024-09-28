90df257644789a9e85f0f7c03920bb2bd7545675
alexlegreat <yabrofaj@mail.uc.edu>
2024-09-27 20:05:55 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner age = new Scanner(System.in);
+        int ageInt = 0;
+
+        System.out.println("Enter your age here: ");
+        ageInt = age.nextInt();
+        age.nextLine();
+        if(ageInt >= 21)
+        {
+            System.out.println("You are older than 21! You get a wristband! " );
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
